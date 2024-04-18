ecc49fe9c06c219e3f3a524aaf3a2d79899b4bee
romulosilvestre <romulo.cesardf@hotmail.com>
2024-04-17 22:04:35 -0300

    conector

--- main.py
+++ main.py
@@ -2,6 +2,22 @@
 #o make_response geralmente gera em JSON
 from flask import Flask,make_response,jsonify,request
 from dados import Materias
+import mysql.connector #importando o conector
+
+#variável para conexão
+#host - é o servidor
+#user - usuário
+#senha - root
+#database - enem
+mydb = mysql.connector.connect(
+   host = 'localhost',
+   user = 'root',
+   password= 'root',
+   database = 'enemteste',
+)
+#SQL no Python
+#SELECT * FROM materias
+#FIXME:INSERT INTO ... (parei aqui! 17/04)
 
 #FIXME: Flask é um classe
 #instâciando o objeto app
@@ -13,9 +29,17 @@ app = Flask("__name__")
 #POST(inserir),PUT(atualizar),GET,DELETE
 @app.route('/materias',methods=['GET'])
 def get_materias():
+    #NOTE: crie o cursor para executar SQL
+    my_cursor = mydb.cursor()
+    my_cursor.execute("SELECT * FROM materias")
+    #retornar a lista de todas as materiais
+    minhas_materiais=my_cursor.fetchall()
     #response
     return make_response(
-        jsonify(Materias)
+        jsonify(
+            mensagem = "lista de matérias cadastradas",
+            dados = minhas_materiais
+        )
     ) 
 
 @app.route("/")
